51ab018ff4199ea5d3dba118fbfe8a55fac0936e
khajamoinuddin_er <khajamoinuddin_er@UKLDN966>
2020-08-03 12:11:46 +0100

    Done

---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private const string Expected = "Hello World! This is test project";
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                ConsoleApp.Program.Main();
+
+                var result = sw.ToString().Trim();
+                Assert.AreEqual(Expected, result);
+            }
+        }
+    }
+}
